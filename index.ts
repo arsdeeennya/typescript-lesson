40474c9a654a839275895e134844c2650daebeff
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-05-01 14:32:47 +0900

    型

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
 const hello: string = 'hello';
 console.log(hello);
+
+// カーソル合わせると,tsコンパイラが動いて方を表示してくれる(tscが動いてる)
+let hasValue: boolean = false;
+let count: number = 10;
+let single: string = "hello";
+
