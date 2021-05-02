85f4e18db5e26cb28dda37b6ff8ab65558b5416f
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-05-02 17:07:27 +0900

    kata matome

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 var hello = 'hello';
 console.log(hello);
+// カーソル合わせると,tsコンパイラが動いて方を表示してくれる(tscが動いてる)
+//　型注釈
+var hasValue = false;
+var count = 10;
+var single = "hello";
+// let hello: string;
+// hello = 2;
+// hello = 'hello';
+// hello = true;
+// 型推論（基本的には推論を使う、宣言時は型注釈）
+var num = 1;
+// オブジェクトの型推論（これもカーソル合わせれば型見れる）
+var person = {
+    name: {
+        first: 'Jack',
+        last: 'Smith'
+    },
+    age: 21
+};
+// 配列
+var fruits = ['りんご', 'ぶどう', 'なし'];
+fruits.push[22222];
+console.log(fruits);
